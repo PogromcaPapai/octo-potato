69ffce06e08b6faf00f67ef6b255c70c4c3c7c38
Jakub Dakowski <jakubdakowski@gmail.com>
2021-07-05 23:16:27 +0200

    funkcja do zapisywania

--- bib_yt.py
+++ bib_yt.py
@@ -18,5 +18,10 @@ def do_until_no_next(func: Callable, *args, **kwargs):
         next_token = wyniki.nextPageToken
     return wyniki
 
-def save_file(url: str, comments: list[str]):
-    pass
+def save_file(name: str, url: str, comments: list[str]):
+    header = f'<COSAR ROBOT="" DOH="" PORTAL="" PRESENTATION="" AUTHOR="" INTERACTION="" URL="{url}"> \n\tLiczba komentarzy w pliku = {len(comments)}\n'
+    footer = '</COSAR>'
+    with open(name, 'w') as f:
+        f.write(header)
+        f.writelines(("\n\t%s\n" % i.replace('\n', '\n\t') for i in comments))
+        f.write(footer)
