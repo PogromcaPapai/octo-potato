f4c2514b7be9df07e41dbb09ce7fd57b2543e07e
Jakub Dakowski <jakubdakowski@gmail.com>
2021-07-08 16:53:27 +0200

    done

--- reddit.py
+++ reddit.py
@@ -1,3 +1,4 @@
+from genericpath import isfile
 import praw
 from key import REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT
 import streamlit as st
@@ -6,7 +7,7 @@ from bib import save_file, comment_format
 reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET, user_agent=REDDIT_USER_AGENT)
 r_all = reddit.subreddit("all")
 
-def _forest_iterate(commentforest: praw.models.comment_forest.CommentForest, reply_to: str):
+def _forest_iterate(commentforest: praw.models.comment_forest.CommentForest, reply_to: str) -> list[str]:
     comments = []
     for i in commentforest:
         c = [comment_format(i.author, i.body, reply_to)]
@@ -15,7 +16,7 @@ def _forest_iterate(commentforest: praw.models.comment_forest.CommentForest, rep
         comments += c    
     return comments
 
-def get_comments(post: praw.models.Submission, collect_replies: bool):
+def get_comments(post: praw.models.Submission, collect_replies: bool) -> list[str]:
     formatted_comments = []
     post.comments.replace_more(limit=0)
     for comm in post.comments:
@@ -24,20 +25,25 @@ def get_comments(post: praw.models.Submission, collect_replies: bool):
             formatted_comments.extend(_forest_iterate(comm.replies, comm.author))
     return formatted_comments
 
-def download(search_term: str, collect_replies: bool, amount: int):
+def download(search_term: str, collect_replies: bool, amount: int, omit: bool):
     
     with st.spinner("Zbieranie wyników wyszukiwania"):
-        posts = r_all.search(search_term, limit=amount)
+        if amount > 0:
+            posts = r_all.search(search_term, limit=amount)
+        else:
+            posts = r_all.search(search_term, limit=None)
+            amount = 1000 # "Most of reddit’s listings contain a maximum of 1000 items"
         
     with st.spinner("Zbieranie komentarzy i informacji o postach"):
         progress_bar = st.progress(0 / amount)
         warns = []
-        st.write("""
-                 > Finally, note that the value of submission.num_comments may not match up 100% with the number of comments extracted via PRAW.
-                 > This discrepancy is normal as that count includes deleted, removed, and spam comments.
-                 > ~ Dokumentacja paczki PRAW
-                 """)
+        omitted = 0
         for i, post in enumerate(posts):
+            
+            # Pomijanie istniejących plików
+            if omit and isfile(f'reddit/{search_term}/{post.id}.xml'):
+                omitted += 1
+                continue
             
             # Zbieranie danych o poście
             post_id = post.id
@@ -46,16 +52,27 @@ def download(search_term: str, collect_replies: bool, amount: int):
             
             # Pobieranie komentarzy
             comments = get_comments(post, collect_replies)
+            if collect_replies and len(comments) != official_count:
+                warns.append(
+                    f"Dla postu o ID {post_id} odnaleziono {len(comments)} komentarzy, a system zadeklarował {official_count}"
+                )
 
             # Zapisywanie
             save_file(f'reddit/{search_term}', post_id, url, comments, official_count)
+            
             progress_bar.progress((i + 1) / amount)
-        st.warning("\n - ".join(["Ostrzeżenia:"] + warns))
+ 
+    st.info(f"Znaleziono {i+1} postów")           
+    if omitted > 0:
+        warns.append(f"Program pominął {omitted} postów, gdyż ich pliki już istniały.")
+    if warns:
+        st.warning("\n - ".join(["**Ostrzeżenia**:"] + warns))
 
 
 if __name__=="__main__":
-    HASLO_WYSZUKIWANE = 'genshin'
+    HASLO_WYSZUKIWANE = ''
     CZY_UWZGLEDNIAC_ODPOWIEDZI = True
+    CZY_POMIJAC_ISTNIEJACE_PLIKI = True
     LICZBA_POSTOW = 100
     
-    download(HASLO_WYSZUKIWANE, CZY_UWZGLEDNIAC_ODPOWIEDZI, LICZBA_POSTOW)
+    download(HASLO_WYSZUKIWANE, CZY_UWZGLEDNIAC_ODPOWIEDZI, LICZBA_POSTOW, CZY_POMIJAC_ISTNIEJACE_PLIKI)
